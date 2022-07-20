b95d573f989d51e91074977200ed56f6d5a45879
denifelixe <deni.victoria@gmail.com>
2022-07-20 17:53:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
